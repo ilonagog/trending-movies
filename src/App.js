1fbee205779b8ad611b8469f0669bc094479d5ad
ilona <gogiashvili.ilona@gmail.com>
2023-02-14 10:33:15 -0500

    added genres array in db.json file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
 import './App.css';
 import Home from './components/Home';
+import { useState } from "react"
+import NavBar from './components/NavBar';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+
+
+  const handleDarkMode = () => {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
+
   return (
-    <div className="App">
+
+    <div className={isDarkMode ? "App" : "App light"}>
+      <NavBar isDarkMode={isDarkMode} handleDarkMode={handleDarkMode} />
       <Home />
 
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
-const NavBar = () => {
+function NavBar({ isDarkMode, handleDarkMode }) {
+    const buttonText = isDarkMode ? "Light Mode" : "Dark Mode";
     return (
-        <div>
-
-        </div>
+        <header >
+            <nav>
+                <div className="navigation">
+                    <Link className="button " to="/">
+                        Home
+                    </Link>
+                    <Link className="button" to="/movies">
+                        Movies
+                    </Link>
+                    <Link className="button" to="/genres">
+                        Genres
+                    </Link>
+                    <Link className="button" to="/movie/new">
+                        Add New Movie
+                    </Link>
+                    <button className='button' onClick={handleDarkMode}>{buttonText}</button>
+                </div>
+            </nav>
+        </header>
     )
 }
 export default NavBar
