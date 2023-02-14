ede03538489dcace3d4e51b8f637d7f54474114f
ilona <gogiashvili.ilona@gmail.com>
2023-02-14 00:48:28 -0500

    added videos on Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import trailerVideos from './Videos'
 
 const Home = () => {
+    const handleVideos = trailerVideos.map(video => {
+        return (
+            <div className="container">
+                <iframe className="videos" src={video.embedUrl} title={video.title} id={video.id} allowFullScreen frameBorder="0"></iframe>
+            </div>
+        )
+    })
+
     return (
         <div>
-
+            <div style={{ margin: "1rem 0" }}>
+                <a className="button" href="/movies">
+                    View All Movies
+                    <br></br>
+                </a>
+                <br></br>
+                <h3 style={{ fontSize: "3rem" }}>Upcomming Movie Trailers : </h3>
+                <br></br>
+                <div >
+                    {handleVideos}
+                </div>
+            </div>
         </div>
-    )
+
+    );
+
 }
 export default Home
---
+++ src/components/Videos.js
@@ -0,0 +1,18 @@
+const trailerVideos = [
+    {
+        id: "1",
+        title: "First Trailer",
+        embedUrl: "https://www.youtube.com/embed/M6YfhX83Cj8",
+
+    }, {
+        id: "2",
+        title: "Second Trailer",
+        embedUrl: "https://www.youtube.com/embed/QT2a4mYkakM"
+    }, {
+        id: "3",
+        title: "Third Trailer",
+        embedUrl: "https://www.youtube.com/embed/WWWDskI46Js"
+    }
+
+]
+export default trailerVideos;
