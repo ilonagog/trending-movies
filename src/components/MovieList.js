617aa93232d73d59f7e7c7f68a1f0557753a0712
ilona <gogiashvili.ilona@gmail.com>
2023-02-14 00:26:12 -0500

    created several components

---
+++ src/components/MovieList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const MovieList = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default MovieList
---
+++ src/components/NewMovieForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NewMovieForm = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default NewMovieForm
