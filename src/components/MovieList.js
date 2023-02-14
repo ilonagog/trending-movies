edbebc8a7000dd049636a371ca3bf64f6cbc5128
ilona <gogiashvili.ilona@gmail.com>
2023-02-14 11:14:45 -0500

    added input for search in MovieList.js, added state to search movies, created a function where filtered movie included toLowerCase

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import { useState } from 'react'
 import MoviePage from './MoviePage'
 
 function MovieList({ movies }) {
+    const [search, setSearch] = useState("");
 
-    const movieListPage = movies.map(movie => <MoviePage key={movie.id} movie={movie} />)
+    const searchResults = movies.filter((movie) => {
+        return movie.title.toLowerCase().includes(search.toLowerCase())
+    })
+    const movieListPage = searchResults.map(movie => <MoviePage key={movie.id} movie={movie} />)
+    const handleChange = (e) => setSearch(e.target.value)
+
     return (
         <div>
+            <input type="text" id="search" placeholder="     Search....    🔍" onChange={handleChange} />
             {movieListPage}
 
         </div>
