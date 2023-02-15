776f87eb012484143daf0c352ad5a791ed2721a5
ilona <gogiashvili.ilona@gmail.com>
2023-02-15 13:34:00 -0500

    created Trailers.js component , created array of movie posters to make a horizontal scrolling of movie posters

--- src/components/Trailers.js
+++ src/components/Trailers.js
@@ -1,3 +1,65 @@
-// const [
-//     { }, { }, { }, { }, { }, { }, { }
-// ]
+export const trailers = [
+    {
+        id: "1",
+        img: "https://m.media-amazon.com/images/M/MV5BODI0ZTljYTMtODQ1NC00NmI0LTk1YWUtN2FlNDM1MDExMDlhXkEyXkFqcGdeQXVyMTM0NTUzNDIy._V1_FMjpg_UX1000_.jpg"
+    },
+    {
+        id: "2",
+        img: "https://m.media-amazon.com/images/M/MV5BOTEwYTkzMTMtODEzNC00YWM2LTgxNDMtZWNkNTQzNDVjMWQ2L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMDY4NTQ1MA@@._V1_.jpg"
+    }, {
+        id: "3",
+        img: "https://m.media-amazon.com/images/M/MV5BMjA1MjYyNDkxN15BMl5BanBnXkFtZTgwMTgxODAwOTE@._V1_FMjpg_UX1000_.jpg"
+    }, {
+        id: "4",
+        img: "https://m.media-amazon.com/images/M/MV5BMjljNzE0OTItM2Q1MS00MTUyLTg4YjgtMzUzZDRkMzkyN2NiXkEyXkFqcGdeQXVyODU2MDg1NzU@._V1_FMjpg_UX1000_.jpg"
+    }, {
+        id: "5",
+        img: "https://cdn.cinematerial.com/p/297x/wf55ztco/the-holiday-movie-poster-md.jpg?v=1456819010"
+    }, {
+        id: "6",
+        img: "https://m.media-amazon.com/images/M/MV5BMjE0MTk3MjE2Nl5BMl5BanBnXkFtZTcwMjE0ODA0NA@@._V1_.jpg"
+    }, {
+        id: "7",
+        img: "https://m.media-amazon.com/images/M/MV5BZDQyODUwM2MtNzA0YS00ZjdmLTgzMjItZWRjN2YyYWE5ZTNjXkEyXkFqcGdeQXVyMTI2MzY1MjM1._V1_.jpg"
+    }, {
+        id: "8",
+        img: "https://cdn11.bigcommerce.com/s-yzgoj/images/stencil/1280x1280/products/268821/4556789/apiihy1mm__31528.1625622408.jpg?c=2"
+    }, {
+        id: "9",
+        img: "https://m.media-amazon.com/images/M/MV5BMTA3MDkxOTc4NDdeQTJeQWpwZ15BbWU4MDAxNzgyNTQz._V1_.jpg"
+    }, {
+        id: "10",
+        img: "https://m.media-amazon.com/images/M/MV5BMGUyM2ZiZmUtMWY0OC00NTQ4LThkOGUtNjY2NjkzMDJiMWMwXkEyXkFqcGdeQXVyMzY0MTE3NzU@._V1_FMjpg_UX1000_.jpg"
+    }, {
+        id: "11",
+        img: "https://m.media-amazon.com/images/M/MV5BYTJiMWFiNzgtMDQxZi00NTFiLWE2YjMtNTA5NzY5YzQxZjNjXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_.jpg"
+    }, {
+        id: "12",
+        img: "https://m.media-amazon.com/images/M/MV5BN2EwM2I5OWMtMGQyMi00Zjg1LWJkNTctZTdjYTA4OGUwZjMyXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
+    }, {
+        id: "13",
+        img: "https://m.media-amazon.com/images/M/MV5BNzA1Njg4NzYxOV5BMl5BanBnXkFtZTgwODk5NjU3MzI@._V1_.jpg"
+    }, {
+        id: "14",
+        img: "https://m.media-amazon.com/images/M/MV5BZDUxMWNlMTUtYTljZS00MTE0LTlkYjktOTU1ODZjYzBhMTk0XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg"
+    }, {
+        id: "15",
+        img: "https://m.media-amazon.com/images/M/MV5BYTRiNDQwYzAtMzVlZS00NTI5LWJjYjUtMzkwNTUzMWMxZTllXkEyXkFqcGdeQXVyNDIzMzcwNjc@._V1_.jpg"
+    }, {
+        id: "16",
+        img: "https://m.media-amazon.com/images/M/MV5BODk4ZjU0NDUtYjdlOS00OTljLTgwZTUtYjkyZjk1NzExZGIzXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg"
+    }, {
+        id: "17",
+        img: "https://m.media-amazon.com/images/M/MV5BMTM3MDc3MjA1OF5BMl5BanBnXkFtZTcwMjY1NDAyMw@@._V1_.jpg"
+    },
+    {
+        id: "18",
+        img: "https://m.media-amazon.com/images/M/MV5BNzYyODAwMzQtY2MyMy00ZTgzLTkxMmMtZWE4NWZkMmFhYWIwXkEyXkFqcGdeQXVyMTA2ODkwNzM5._V1_.jpg"
+    }, {
+        id: "19",
+        img: "https://m.media-amazon.com/images/M/MV5BMjA3NzI1ODc1MV5BMl5BanBnXkFtZTcwMzI5NjQwNg@@._V1_.jpg"
+    }, {
+        id: "20",
+        img: "https://m.media-amazon.com/images/M/MV5BMTk5OGFiYzktMTkwNS00ZjQ5LWFkNGQtNGM0MjA4MWE2MDIwXkEyXkFqcGdeQXVyODY1NDk1NjE@._V1_.jpg"
+    },
+]
